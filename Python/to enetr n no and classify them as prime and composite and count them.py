e5f9f27d0792dad33fa2ac29c025494f7965b0c6
muskanagarwa <73011061+muskanagarwa@users.noreply.github.com>
2020-10-17 13:04:19 +0530

    Update to enetr n no and classify them as prime and composite and count them.py

--- Python/to enetr n no and classify them as prime and composite and count them.py
+++ Python/to enetr n no and classify them as prime and composite and count them.py
@@ -4,16 +4,14 @@ no=0
 c=0
 iscomp=0
 while (no!=-1):
-    no=int(input("\n Enter the no"))
+    no=int(input(" \n Enter the no "))
     iscomp=0
     for c in range(2,no):
         if (no%c==0):
             iscomp=1
             break
             
-
-
-    if (iscomp==1):
+        if (iscomp==1):
         cno+=1
         print(cno , end ="  ",)
         
